7b9d29b90a6954020d72095e8a5c9f03310f8821
ramy sabry <ramyalsabry.dev@gmail.com>
2024-06-10 19:56:33 +0300

    edit

--- src/components/Fingerprint/Fingerprint.jsx
+++ src/components/Fingerprint/Fingerprint.jsx
@@ -3,17 +3,25 @@ import FingerImg from "../../assets/fingerPrint/student.jpg";
 
 const Fingerprint = () => {
   return (
-    <div className="container mx-auto py-16" data-aos="fade-up" ata-aos-duration="2000">
+    <div
+      className="container mx-auto py-16"
+      data-aos="fade-up"
+      ata-aos-duration="2000"
+    >
       <div className="flex items-center justify-between w-full">
         <div className="w-[60%] relative z-50">
-          <h2 className="text-3xl font-semibold text-[#8f1537]">بصمة الجزري</h2>
+          <h2 className="text-3xl font-semibold text-[#8f1537]">
+            ALJAZARI'S WAY{" "}
+          </h2>
           <p className="text-xl text-[#244b5a] my-10">
-            تعتبر مدارس الجزري الدولية، التي أرست أسس نظام تعليمي فريد من نوعه
-            بخطوات أصيلة في رحلة العلم والمعرفة، رائدة في التعليم الدولي. حيث
-            تقدم مدارس الجزري الدولية لطلبتها الفرصة لاكتشاف اهتماماتهم وقدراتهم
-            من خلال برامج مصممة باحترافية من تطوير المدارس
+            Aljazari International Schools, which laid the foundations of a
+            unique educational system with original steps in the journey of
+            science and knowledge, is a pioneer in international Education.
+            Aljazari International Schools offers students the opportunity to
+            discover their interests and abilities with professionally designed
+            programs developed by the schools.
           </p>
-          <button className="button">اقرأ المزيد</button>
+          <button className="button">READ MORE </button>
         </div>
         <div className="relative w-[40%] flex justify-end">
           <div className="bg-[#f1f2f2] rounded-full absolute top-16 right-48 w-[400px] h-[400px]"></div>
---
+++ src/components/News/News.jsx
@@ -0,0 +1,185 @@
+import React, { Fragment } from "react";
+import News_1 from "../../assets/news/WhatsApp-Image-2024-02-28-at-2.24.49-PM.jpeg";
+import News_2 from "../../assets/news/News-Austrilan.jpg";
+import News_3 from "../../assets/news/02.jpg";
+
+const News = () => {
+  const newsData = [
+    {
+      image: News_1,
+      title: "Announcement and Congratulations to the Scholarship Winners!",
+      disc: "We are thrilled to announce the recipients of this year's scholarship! After careful consideration, 7 outstanding students have been selected for their exceptional achievements, dedication to academic excellence, and impactful contributions to our community....",
+      color: "#8f1537",
+    },
+    {
+      image: News_2,
+      image_info:
+        "Fostering Global Educational Partnerships: Aljazari’s Historic Agreement with Western Australian Ministry of Education",
+    },
+    {
+      image: News_2,
+      image_info:
+        "Fostering Global Educational Partnerships: Aljazari’s Historic Agreement with Western Australian Ministry of Education",
+    },
+    {
+      image: News_3,
+      title: "Announcement and Congratulations to the Scholarship Winners!",
+      disc: "We are thrilled to announce the recipients of this year's scholarship! After careful consideration, 7 outstanding students have been selected for their exceptional achievements, dedication to academic excellence, and impactful contributions to our community....",
+      color: "#faa61a",
+    },
+    {
+      image: News_1,
+      title: "Announcement and Congratulations to the Scholarship Winners!",
+      disc: "We are thrilled to announce the recipients of this year's scholarship! After careful consideration, 7 outstanding students have been selected for their exceptional achievements, dedication to academic excellence, and impactful contributions to our community....",
+      color: "#8f1537",
+    },
+    {
+      image: News_2,
+      image_info:
+        "Fostering Global Educational Partnerships: Aljazari’s Historic Agreement with Western Australian Ministry of Education",
+    },
+    {
+      image: News_2,
+      image_info:
+        "Fostering Global Educational Partnerships: Aljazari’s Historic Agreement with Western Australian Ministry of Education",
+    },
+    {
+      image: News_3,
+      title: "Announcement and Congratulations to the Scholarship Winners!",
+      disc: "We are thrilled to announce the recipients of this year's scholarship! After careful consideration, 7 outstanding students have been selected for their exceptional achievements, dedication to academic excellence, and impactful contributions to our community....",
+      color: "#faa61a",
+    },
+  ];
+
+  return (
+    <div className="container py-28 m-auto news">
+      <h2 className="text-6xl text-[#8f1537] font-bold text-center">
+        NEWS & Announcement
+      </h2>
+      <div className="flex gap-5 mx-auto my-16">
+        <div>
+          {newsData &&
+            newsData.slice(0, 2).map((item, index) => (
+              <div
+                key={index}
+                className={`${
+                  item.image_info ? "h-64" : "h-[500px]"
+                } overflow-hidden rounded-2xl relative mb-5`}
+              >
+                <div className={`${item.image_info ? "h-64" : "h-1/2"}`}>
+                  <img src={item.image} alt="news" className="h-full" />
+                </div>
+                {item.image_info ? (
+                  <Fragment>
+                    <div className="text_gradient absolute -bottom-28 text-white w-full text-start px-5 font-bold text-2xl z-30 cursor-pointer">
+                      {item.image_info}
+                    </div>
+                    <div className="linear_gradient absolute top-0 left-0 w-full h-full cursor-pointer"></div>
+                  </Fragment>
+                ) : (
+                  <div className="p-6" style={{ backgroundColor: item.color }}>
+                    <h2 className="text-xl text-white font-semibold mb-2">
+                      {item.title}
+                    </h2>
+                    <p className="text-white">{item.disc}</p>
+                  </div>
+                )}
+              </div>
+            ))}
+        </div>
+        <div>
+          {newsData &&
+            newsData.slice(2, 4).map((item, index) => (
+              <div
+                key={index}
+                className={`${
+                  item.image_info ? "h-64" : "h-[500px]"
+                } overflow-hidden rounded-2xl relative mb-5`}
+              >
+                <div className={`${item.image_info ? "h-64" : "h-1/2"}`}>
+                  <img src={item.image} alt="news" className="h-full" />
+                </div>
+                {item.image_info ? (
+                  <Fragment>
+                    <div className="text_gradient absolute -bottom-28 text-white w-full text-start px-5 font-bold text-2xl z-30 cursor-pointer">
+                      {item.image_info}
+                    </div>
+                    <div className="linear_gradient absolute top-0 left-0 w-full h-full cursor-pointer"></div>
+                  </Fragment>
+                ) : (
+                  <div className="p-6" style={{ backgroundColor: item.color }}>
+                    <h2 className="text-xl text-white font-semibold mb-2">
+                      {item.title}
+                    </h2>
+                    <p className="text-white">{item.disc}</p>
+                  </div>
+                )}
+              </div>
+            ))}
+        </div>
+        <div>
+          {newsData &&
+            newsData.slice(4, 6).map((item, index) => (
+              <div
+                key={index}
+                className={`${
+                  item.image_info ? "h-64" : "h-[500px]"
+                } overflow-hidden rounded-2xl relative mb-5`}
+              >
+                <div className={`${item.image_info ? "h-64" : "h-1/2"}`}>
+                  <img src={item.image} alt="news" className="h-full" />
+                </div>
+                {item.image_info ? (
+                  <Fragment>
+                    <div className="text_gradient absolute -bottom-28 text-white w-full text-start px-5 font-bold text-2xl z-30 cursor-pointer">
+                      {item.image_info}
+                    </div>
+                    <div className="linear_gradient absolute top-0 left-0 w-full h-full cursor-pointer"></div>
+                  </Fragment>
+                ) : (
+                  <div className="p-6" style={{ backgroundColor: item.color }}>
+                    <h2 className="text-xl text-white font-semibold mb-2">
+                      {item.title}
+                    </h2>
+                    <p className="text-white">{item.disc}</p>
+                  </div>
+                )}
+              </div>
+            ))}
+        </div>
+        <div>
+          {newsData &&
+            newsData.slice(6, 8).map((item, index) => (
+              <div
+                key={index}
+                className={`${
+                  item.image_info ? "h-64" : "h-[500px]"
+                } overflow-hidden rounded-2xl relative mb-5`}
+              >
+                <div className={`${item.image_info ? "h-64" : "h-1/2"}`}>
+                  <img src={item.image} alt="news" className="h-full" />
+                </div>
+                {item.image_info ? (
+                  <Fragment>
+                    <div className="text_gradient absolute -bottom-28 text-white w-full text-start px-5 font-bold text-2xl z-30 cursor-pointer">
+                      {item.image_info}
+                    </div>
+                    <div className="linear_gradient absolute top-0 left-0 w-full h-full cursor-pointer"></div>
+                  </Fragment>
+                ) : (
+                  <div className="p-6" style={{ backgroundColor: item.color }}>
+                    <h2 className="text-xl text-white font-semibold mb-2">
+                      {item.title}
+                    </h2>
+                    <p className="text-white">{item.disc}</p>
+                  </div>
+                )}
+              </div>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default News;
